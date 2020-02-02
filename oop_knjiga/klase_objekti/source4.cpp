99e2cc3ef7fe233fc193a052d660db770d1db14c
drunkking <dusan.kuburic@outlook.com>
2020-02-02 01:56:56 +0100

    klase, objekti part1 knjiga

---
+++ oop_knjiga/klase_objekti/source4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Human
+{
+private:
+	string name;
+	int age;
+
+public:
+	Human()
+	{
+		age = 0;
+		cout << "Default constructor: name and age not set" << endl;
+	}
+
+	Human(string humansName, int humansAge)
+	{
+		name = humansName;
+		age = humansAge;
+		cout << "Overloaded constructor creates ";
+		cout << name << " of " << age << " years " << endl;
+	}
+};
+
+int main()
+{
+	Human firstMan;
+	Human firstWoman("Eve", 20);
+
+	return 0;
+}
